8d3e16f09bd89348484548215c8555cb974daad8
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2023-02-21 17:12:18 +0100

    finished the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
-    <div className='hero-container'>
-        <div className="hero-text"></div>
-        <div className="hero-image"></div>
+    <div className="hero-container">
+      <div className="hero-text">
+        <h1>
+          Be The Fastest In Delevering Your{" "}
+          <span className="red-text">Food</span>
+        </h1>
+        <p className="body-small">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+          quod soluta! Esse in delectus consequatur ipsa aspernatur, eos nihil
+          debitiis itaque.
+        </p>
+        <div className="btns">
+          <button className="btn">Order Now</button>
+          <button className="btn btn-ghost">Become Restaurent</button>
+        </div>
+      </div>
+      <div className="hero-img">
+        <img src="./images/hero-bg.png" alt="hero background" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
