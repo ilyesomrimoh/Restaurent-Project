7df7a9e5e59fe0556763171bab5c187af05507d2
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2023-02-21 16:14:03 +0100

    added images folder

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='hero-container'>
+        <div className="hero-text"></div>
+        <div className="hero-image"></div>
+
+    </div>
+  )
+}
+
+export default Hero
