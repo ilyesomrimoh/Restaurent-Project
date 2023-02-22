1a8b88e1454c8da847f870761d5cdf38a43c05a7
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2023-02-22 17:35:10 +0100

    testtt

---
+++ src/components/Testmonials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Testmonials() {
+  return (
+    <div className='testi-container' >
+        <h3 className='body-large'>
+            
+        </h3>
+    </div>
+  )
+}
+
+export default Testmonials
