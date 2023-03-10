2d11bc3a22e851396f23f258e9f17230d4b22442
HaythemSdn <h.saidani@esi-sba.dz>
2023-03-10 15:32:22 +0000

    up to date

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 // import LandingPage from "./Views/LandingPage";
 import Login from "./components/SignUp_Login/Login";
-
+import ErrorPage from "./Views/ErrorPage";
+import SignUp from "./components/SignUp_Login/SignUp";
+import LandingPage from "./Views/LandingPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 function App() {
   const router = createBrowserRouter([
     {
-      path:'/',
-      element:<LandingPage/>,
-      errorElement: <ErrorPage/>
+      path: "/",
+      element: <LandingPage />,
+      errorElement: <ErrorPage />,
     },
     {
-      path:'signup',
-      element:<SignUp/>,
+      path: "signup",
+      element: <SignUp />,
     },
     {
-      path:'login',
-      element:<Login/>,
+      path: "login",
+      element: <Login />,
     },
-  ])
+  ]);
   return (
     <>
-      <Login />
+      <RouterProvider router={router} />
     </>
   );
 }
