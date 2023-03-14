efd9ede22ebb5c99eaee9248d005ef11110e7bf6
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2023-03-14 11:12:15 +0100

    css hell

---
+++ src/config/firebas_config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDl0m6LaX62wLfBo5xjp2G3kVBM65gK5V0",
+  authDomain: "clickool.firebaseapp.com",
+  projectId: "clickool",
+  storageBucket: "clickool.appspot.com",
+  messagingSenderId: "444306253644",
+  appId: "1:444306253644:web:a29ba90940ec7f892dd1e9"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const googleProvider = new GoogleAuthProvider();
