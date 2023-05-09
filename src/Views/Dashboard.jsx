48818b51fbbe57c02fd15895143e9e41359f07bf
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2023-05-09 11:04:51 +0100

    modifieed sidebar component (not finished)

--- src/Views/Dashboard.jsx
+++ src/Views/Dashboard.jsx
@@ -21,7 +21,7 @@ const Dashboard = () => {
      
         <Sidebar />
         <div className="flex-1 p-4">
-          {user && <h1 style={{textAlign : 'center', width: '100%', height: '100vh',paddingTop: '20vh'}}>Welcome to your Dashboard <span className='red-text' >{user.email}</span> {user.displayName && `your name is ${user.displayName}`}</h1>}
+          {user && <h1 className='text-center text-lg p-20'>Welcome to your Dashboard <span className='red-text' >{user.email}</span> {user.displayName && `your name is ${user.displayName}`}</h1>}
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mauris quis velit consequat aliquam. Donec tincidunt malesuada dui ut venenatis. Proin commodo luctus lorem ut porttitor. Suspendisse bibendum commodo enim, at interdum sapien dapibus eget. Nam vel erat sapien. Duis convallis nisi eget velit luctus, ut suscipit nunc convallis. Nullam dignissim, arcu eget blandit pulvinar, velit velit vehicula enim, vel lobortis risus tortor non magna. Suspendisse potenti. Nam pretium consectetur tellus, in varius odio lobortis eu. Donec eget felis et augue eleifend ultricies in eu dolor. Pellentesque sit amet augue ut ante placerat laoreet. Nulla facilisi.</p>
         </div>
 
--- src/components/Dashboard/Sidebar.jsx
+++ src/components/Dashboard/Sidebar.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
 
 function Sidebar() {
+    const {user} = useContext(UserContext);
   return (
     //tailwind class to make a sidebar container
-    <div className="w-64 bg-gray-800 text-white p-4">
-        <h1 className="text-lg font-bold">My Sidebar</h1>
-        <ul className='li:text-white'>
-        <li><Link to="#" className="block py-2 ">Item 1</Link></li>
-        <li><Link to="#" className="block py-2">Item 2</Link></li>
-        <li><Link to="#" className="block py-2">Item 3</Link></li>
+    <div id='sidebar' className="w-64 bg-[var(--primary-color)] text-white p-4">
+        <div className='profile'>
+            <div className="prof-img m-auto w-[100px]">
+                <img src="./images/icons/avatar.png" className="w-full" alt="" />
+            </div>
+            <div className="prof-info text-center">
+                <h6 className='text-2xl'>Your name</h6>
+                <p className='text-xs'>{ user && user.email}</p>
+            </div>
+        </div>
+        <ul className='text-white !important'>
+            <li><Link to="#" className="block py-2 ">Item 1</Link></li>
+            <li><Link to="#" className="block py-2">Item 2</Link></li>
+            <li><Link to="#" className="block py-2">Item 3</Link></li>
         </ul>
   </div>
     
