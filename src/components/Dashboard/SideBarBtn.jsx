e7901c7383438af2a75dc5394b3569a20bb20fb6
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2023-05-12 22:34:05 +0100

    Added closing sidebar and icons to OverviewCard components

--- src/components/Dashboard/SideBarBtn.jsx
+++ src/components/Dashboard/SideBarBtn.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-const SideBarBtn = ({title,img , img_alt}) => {
+const SideBarBtn = ({opn,title,img , img_alt}) => {
 //   const changeSrc = (e,id) => {
 //     const imgg = e.target.querySelector("img");
 //     if(id === 1 && imgg){
@@ -11,13 +11,9 @@ const SideBarBtn = ({title,img , img_alt}) => {
 // }
   return (
     //onMouseOver={(e)=> changeSrc(e,1)} onMouseLeave={(e)=> changeSrc(e,2)}
-    <div  className="side-bar-btn flex gap-4 justify-start items-center p-3 my-2 w-44 h-18 rounded-xl">
-      <img className='w-6' src={img} alt={img_alt}/>
-
-
-
-
-      <h2  className='text-lg font-semibold tracking-wide ' >{title}</h2>
+    <div  className={`side-bar-btn flex gap-4 overflow-hidden items-center p-3 my-2 ${opn ? 'w-44' : 'w-full'} h-18 rounded-xl`}>
+      <img className={`${opn ? 'w-[36px]' : 'w-[50px]'} transitio-all delay-250`} src={img} alt={img_alt}/>
+      <h2  className={`icon-header text-lg font-semibold tracking-wide ${opn ? '' : 'hidden'} transitio-all delay-150`} >{title}</h2>
     </div>
 
   )
