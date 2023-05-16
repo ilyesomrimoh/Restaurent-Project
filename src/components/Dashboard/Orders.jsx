a46d5dd6a0c7422c25f56ae78ff318d30ac08b1a
Ilyeshaddad337 <i.haddad@esi-sba.dz>
2023-05-16 01:33:04 +0100

    Orders are linked to database now\nTODOS:\n-loop through items of each order

--- src/components/Dashboard/Orders.jsx
+++ src/components/Dashboard/Orders.jsx
@@ -1,27 +1,55 @@
-import React from 'react'
+import   { useEffect,useState } from 'react'
 import OverviewCard from './OverviewCard'
 import NavBar from './NavBar'
 import OrderCard from './OrderCard'
+import { db } from '../../config/firebase_config'
+import { collection, onSnapshot} from 'firebase/firestore'
 const Orders = () => {
+  const [orders, setOrders] = useState([{id:"21645645"}])
+  const [loading, setLoading] = useState(true);
+
   const itms = [{"img":"/images/icons/avatar.png","name":"Pizza"}]
+  useEffect(() => {
+    const ordersRef = collection(db,"Orders");
+    setLoading(true);
+    const unsub = onSnapshot(ordersRef, (snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      setOrders(data);
+      setLoading(false);
+    } )
+    return ()=> { unsub()};
+  }, [])
+  const getDate = (order) => {  
+    const date = new Date(order.createdDate?.seconds * 1000)
+    return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+  }
+  const getHour = (order) => {  
+    const date = new Date(order.createdDate?.seconds )
+    return `${date.getHours()}:${date.getMinutes()}`
+  }
   return (
     <div className='w-full'>
       < NavBar />
-      <h2 className='font-bold m-4 mb-7'>Orders</h2>
+      <h2 onClick={()=> console.log(orders[0])} className='font-bold m-4 mb-7'>Orders</h2>
       <h3 className='text-xl font-bold ml-8  mb-5'>Overview</h3>
      <div className="flex gap-6 w-fit mr-auto ml-auto flex-wrap pl-6 mb-16">
      <OverviewCard title="Pending Orders" content="320"  />
       <OverviewCard title="Active Orders" content="43000"  />
       <OverviewCard title="Canceled Orders" content="12"  />
      </div>
-     <div className="tabs flex justify-start items-center gap-5 ml-8 mb-8">
+     {loading ? <h1 className='text-center text-2xl font-bold'>Loading...</h1> : (<><div className="tabs flex justify-start items-center gap-5 ml-8 mb-8">
         <div className="tab active-tab">Pending Orders</div>
         <div className="tab">Active Orders</div>
         <div className="tab">Canceled Orders</div>
      </div>
      <div className="orders flex flex-col items-center justify-start">
-        <OrderCard OrderId={"246963"} OrderDate={"02-05-2023"} OderTime={"18:06"} status={"active"} items={itms} OrderPrice={"2500DA"} UserPhone={"0555132595"}/>
-     </div>
+          {orders.map((order) => (
+            <OrderCard key={order.id} OrderId={order.id} OrderDate={getDate(order)} OderTime={getHour(order)} items={itms} OrderPrice={order.total} status={order.status || "pending"} UserPhone={order.userPhone}/>))}        
+
+     </div></>)}
     </div>
   )
 }
--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -1,16 +1,39 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider  } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDl0m6LaX62wLfBo5xjp2G3kVBM65gK5V0",
-  authDomain: "clickool.firebaseapp.com",
-  projectId: "clickool",
-  storageBucket: "clickool.appspot.com",
-  messagingSenderId: "444306253644",
-  appId: "1:444306253644:web:a29ba90940ec7f892dd1e9"
+  apiKey: "AIzaSyCwv-aQYgvWDgR_0UloDcbLMFqHFLytoOM",
+  authDomain: "restaurant-marketplace-60929.firebaseapp.com",
+  projectId: "restaurant-marketplace-60929",
+  storageBucket: "restaurant-marketplace-60929.appspot.com",
+  messagingSenderId: "90174131186",
+  appId: "1:90174131186:web:6adc6d3e6033694f6b796b",
+  measurementId: "G-15K0RMTDZY"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider  } from "firebase/auth";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDl0m6LaX62wLfBo5xjp2G3kVBM65gK5V0",
+//   authDomain: "clickool.firebaseapp.com",
+//   projectId: "clickool",
+//   storageBucket: "clickool.appspot.com",
+//   messagingSenderId: "444306253644",
+//   appId: "1:444306253644:web:a29ba90940ec7f892dd1e9"
+// };
+
+
+
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth(app);
+// export const googleProvider = new GoogleAuthProvider();
